63d8df158d9dd517c772997627bf5aa927d17c10
Lucas Grandviergne <lucasgdve@gmail.com>
2019-10-21 18:31:22 +0200

    lucas commit

---
+++ lib/controller.rb
@@ -0,0 +1,22 @@
+require 'gossip'
+
+
+class ApplicationController < Sinatra::Base
+
+    get '/' do
+        erb :index, locals: {gossips: Gossip.all}
+    end
+
+    get '/gossips/new/' do
+        erb :new_gossip
+    end
+
+
+    post '/gossips/new/' do
+        Gossip.new(params["gossip_author"], params["gossip_content"]).save
+        puts "Trop bien ! Et ceci est ce que l'utilisateur a passé dans le champ gossip_author : #{params["gossip_author"]}"
+        puts "De la bombe, et du coup ça, ça doit être ce que l'utilisateur a passé dans le champ gossip_content : #{params["gossip_content"]}"
+        redirect '/'
+      end
+end
+
